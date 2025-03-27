87d38a596bb5e878782a494a53654da099f9b6f9
Yagho Petini <yaghojp@gmail.com>
2025-03-27 02:24:34 +0000

    [001 - Lex: Configurando o analisador léxico]

---
+++ analise-lexica/teste-anotations.c
@@ -0,0 +1,15 @@
+%{
+    Aqui dentro pode ter as includes e alguns códigos
+    em c
+%}
+
+Expressões regulares
+%%
+    Aqui fica o espaço onde as suas expressões
+    regulares dão "match" com a string encontrada no
+    arquivo.
+%%
+int main(){
+    main padrão de C
+}
+
